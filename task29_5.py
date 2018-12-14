2fa0e3348f7d3e4859c3e57d881557ad0640dc37
denstan93 <denstan93@gmail.com>
2018-12-14 20:32:47 +0300

    task29_5

--- task29_5.py
+++ task29_5.py
@@ -3,5 +3,5 @@ def task29_5(a):
    if re.match("^\d{4}[-/|]\d{2}\-\d{2}\s\d{2}\:\d{2}\:?\d?\d?$", a)is not None:
        return re.match("^\d{4}[-/|]\d{2}\-\d{2}\s\d{2}\:(\d{2})\:?\d?\d?$", a).groups()
    else:
-       raise Exception('это не время, а фигня!!!!')
+       raise Exception('это не время, а фигня!!!')
 print(task29_5('2018-22-13 12:30:40'))
