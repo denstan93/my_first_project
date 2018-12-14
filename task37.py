53014abef0ad8b21e3a2bfdd366e353e268d9656
denstan93 <denstan93@gmail.com>
2018-12-14 21:56:41 +0300

    task37

--- task37.py
+++ task37.py
@@ -5,11 +5,10 @@ print(df.columns)
 print('Всего пассажиров:',len(df['Name']))
 print('Мужчин на борту:',len(df[df['Sex'] == 'male']))
 print('Процент выживаемости:',(len(df[df['Survived'] == 1])/(len(df['Name']))*100))
-if len(df[df['Sex'] == 'male']) > len(df[df['Sex'] == 'famale']):
+if (df[df['Sex'] == 'male'].shape[0]) > (df[df['Sex'] == 'famale'].shape[0]):
     print('Мужчин больше')
 else:
     print('Больше женщин')
-
 print('Процент выживших мужчин:',len(df[(df['Sex'] == 'male') & (df['Survived'] == 1)])/len(df[df['Survived'] == 1])*100)
 n = len(df[df['Pclass'] == 1])
 c = len(df[df['Pclass'] == 2])
